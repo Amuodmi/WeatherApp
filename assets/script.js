42b079e42da40fcbbbc5b13c2ecdb1ee97a14d39
Michelle <michelle.douma@gmail.com>
2022-07-15 13:57:42 -0700

    trying to get the enter button working

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,8 @@
-var apiKey = "96f33839c85744a54cc32451f4cf28cb";
 var previousCities = { city: [] }
 var searchHistoryEl = $('.historySection');
 var searchHistory = JSON.parse(localStorage.getItem('cities'));
 
-// if theres local storage data, history section will print
+//if there is any local storage data, the history section will display beneath the button:
 if (searchHistory) {
     for (var i = 0; i < searchHistory.length; i++) {
         var prevCity = $('<p>').addClass("btn history-button").text(searchHistory[i].city);
@@ -11,27 +10,32 @@ if (searchHistory) {
     }
 }
 
-// city param comes from searchDemCities click, grabs lat lon passes to fetchWeather
+//the city parameter comes from searchCities click, grabs latitude/longitude, and passes that to fetchWeather:
 let weather = {
-    // find your coords
+
+    // find coordinates
     fetchCoords: function (city) {
-        // finds the lon and lat coords based on city name
-        fetch("https://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=5&appid=" + apiKey)
-            // parses data then sets param for fetchWeather
+
+        // finds the longitude and latitude coords based on city name:
+        fetch("https://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=5&appid=96f33839c85744a54cc32451f4cf28cb")
+
+            // parses the data then sets parameter for fetchWeather:
             .then((response) => response.json())
             .then((data) => this.fetchWeather(data));
     },
-    // uses coords to find weather for city then passes data to displayWeather
+    // uses coordinates to find the weather for the city, and then passes that data to displayWeather:
     fetchWeather: function (data) {
         document.querySelector(".city-name").innerText = data[0].name + ", " + data[0].state;
-        // finds weather data based on lon and lat input
+        // finds weather data based on the longitude and latitude:
         fetch("https://api.openweathermap.org/data/2.5/onecall?units=metric&lat="
-            + data[0].lat + "&lon=" + data[0].lon + "&exclude=minutely,hourly&appid=" + apiKey)
-            // takes all that data, parses, and fires displayWeather with data param
+            + data[0].lat + "&lon=" + data[0].lon + "&exclude=minutely,hourly&appid=" + "96f33839c85744a54cc32451f4cf28cb")
+
+            // takes all that data, parses, and inserts displayWeather with data the parameters:
             .then((response) => response.json())
             .then((data) => this.displayWeather(data));
     },
-    // uses data param to find specific endpoints, displays all data to screen including 5 day forecast
+
+    // uses data parameter to find specific endpoints, displays all the data to the screen, including a 5 day forecast:
     displayWeather: function (data) {
         var date = new Date(data.current.dt * 1000).toLocaleString();
         // puts the correct data on the screen
@@ -49,32 +53,35 @@ let weather = {
         } if (data.current.uvi > "6") {
             $(".UVindex").removeClass("UVindexLight UVindexModerate").addClass("UVindexHigh")
         }
-        //create the 5 forecast cards
-        var forecastCards = function () {
+
+        //create the 5 forecast boxes:
+        function forecastBoxes() {
             forecastEl = $(".forecast");
             forecastEl.empty();
-            // loop creates 5 cards with 5 day forecast
+        
+
             for (var i = 1; i < 6; i++) {
-                // converts unix timestamp to date we can read 
+                // dissects timestamp to a date that we can read: 
                 var date = new Date(data.daily[i].dt * 1000).toLocaleDateString();
-                // yeets cards into existence
-                var forecastCardEl = $("<div>").addClass("column forecast-card");
+
+                //Boxes For Weather:
+                var forecastBoxEl = $("<div>").addClass("column forecast-box");
                 var dailyDateEl = $("<h5>").text(date);
                 var dailyIconEl = $("<img>").attr("src", "https://openweathermap.org/img/wn/" + data.daily[i].weather[0].icon + ".png");
                 var dailyTempEl = $("<h6>").text("temp: " + data.daily[i].temp.day + "°C");
                 var dailyHumidityEl = $("<h6>").text("humidity: " + data.daily[i].humidity + "%");
                 var dailyWindEl = $("<h6>").text("wind: " + data.daily[i].wind_speed + "m/s");
-                forecastCardEl.append(dailyDateEl, dailyIconEl, dailyTempEl, dailyHumidityEl, dailyWindEl);
-                forecastEl.append(forecastCardEl);
+                forecastBoxEl.append(dailyDateEl, dailyIconEl, dailyTempEl, dailyHumidityEl, dailyWindEl);
+                forecastEl.append(forecastBoxEl);
             }
         }
-        forecastCards();
+        forecastBoxes();
     }
-}; //weather end
+}; //End of Weather
 
-// really classy name for passing input from search-bar or history section to fetchCoords()
-var searchDemCities = function (event) {
-    // sends search-bar value to fetchCoords, checks for search history, makes new button for city searched
+// Passing Input To fetchCoords:
+  function searchCities(event) {
+    // sends search-bar value to fetchCoords, checks for search history, than makes new button for city searched:
     if (event.target.matches('.search-button')) {
         if ($('.search-bar').val() !== "") {
             const city = $('.search-bar').val().trim();
@@ -83,7 +90,7 @@ var searchDemCities = function (event) {
             if (!savedCities) {
                 savedCities = [];
             }
-            //adds search to local storage
+            //adds search to local storage:
             savedCities.push(previousCities);
             localStorage.setItem('cities', JSON.stringify(savedCities));
             weather.fetchCoords(city);
@@ -91,19 +98,31 @@ var searchDemCities = function (event) {
             searchHistoryEl.append(prevCity);
         }
     }
-    // don't feel like searching? click yo history to see it again
+    //if event to click on the previous search options:
     if (event.target.matches('.history-button')) {
         console.log(event.target.innerHTML)
         weather.fetchCoords(event.target.innerHTML);
+
+        
     }
-    // yeets history from existence 
+    //Delete Button Event:
     if (event.target.matches('.delete-button')) {
         console.log("clicked")
         localStorage.clear();
         searchHistoryEl.remove('.history-button');
         location.reload();
+        (event.key === "Enter"); {
+              $("btn delete-button").click();
+            };
     }
 }
 
-// search. dem. cities.                    dawg.
-$(".city-search").click(searchDemCities);
+var input = document.getElementsByClassName("search-bar");
+input.addEventListener("keypress", function(event) {
+  if (event.keyCode === 13); {
+    //event.preventDefault();
+    document.getElementsByClassName("searchCities").click();
+  }
+  });
+
+$(".city-search").click(searchCities);
